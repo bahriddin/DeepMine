f97e96b5d20a7a3a8b5d97e98cbe29f3e2a18a39
lingtaoj <lingtaoj@student.unimelb.edu.au>
2018-05-02 21:04:53 +1000

    Update: The experience will be removed randomly if the experience buffer is full.

--- Open-AI/Double_Dueling_DQN.py
+++ Open-AI/Double_Dueling_DQN.py
@@ -151,13 +151,15 @@ class RL():
 
 
     class experience_buffer():
-        def __init__(self, buffer_size=50000):
+        def __init__(self, buffer_size=500000):
             self.buffer = []
             self.buffer_size = buffer_size
 
         def add(self, experience):
             if len(self.buffer) + len(experience) >= self.buffer_size:
-                self.buffer[0:(len(experience) + len(self.buffer)) - self.buffer_size] = []
+                offset = np.random.randint(0, int(len(self.buffer)/len(experience)))
+                self.buffer = self.buffer[0:(offset-1)*len(experience)]+self.buffer[offset*len(experience):]
+                # self.buffer[0:(len(experience) + len(self.buffer)) - self.buffer_size] = []
             self.buffer.extend(experience)
 
         def sample(self, size):
