4a9f88485b86a7995006177885c8932412f82e52
Zhenxiang Wang <30854448+ZhenxiangWang@users.noreply.github.com>
2018-05-18 07:58:03 +1000

    Add files via upload

--- Open-AI/A3C/a_0.0001_c_0.001_softmax/a0.0001c0.001_softmax.py
+++ Open-AI/A3C/a_0.0001_c_0.001_softmax/a0.0001c0.001_softmax.py
@@ -1,15 +1,3 @@
-"""
-Asynchronous Advantage Actor Critic (A3C) with continuous action space, Reinforcement Learning.
-
-The Pendulum example. Convergence promised, but difficult environment, this code hardly converge.
-
-View more on [莫烦Python] : https://morvanzhou.github.io/tutorials/
-
-Using:
-tensorflow 1.0
-gym 0.8.0
-"""
-
 import multiprocessing
 import threading
 import tensorflow as tf
